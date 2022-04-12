9016c05befe8b325eb9550430355a5713ca612cc
MCigno94 <mattiacigno27@gmail.com>
2022-04-12 16:53:52 +0200

    added post to DOM

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,26 +14,37 @@ Ogni post dovrà avere le informazioni necessarie per stampare la relativa card:
 */
 
 // Creiamo il nostro array di oggetti che rappresentano ciascun post
-
 let id = 0;
-let fullName = '';
-let photoUser = '';
 let date = getDataAmericanFormat();
-let text = '';
-let likes = 0;
-let image = '';
-
 let arrayPost = [{
-    id,
-    fullName,
-    photoUser,
-    date,
-    text,
-    likes,
-    image
-}]
+        id,
+        fullName: 'Mattia Cigno',
+        photoUser: 'https://picsum.photos/200',
+        date,
+        text: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quis nesciunt aut est commodi! Eos consequuntur, est expedita quo quaerat omnis illo praesentium nemo tempore facere, voluptas perspiciatis exercitationem unde alias!',
+        likes: 30,
+        image: 'https://picsum.photos/200'
+    },
+    {
+        id,
+        fullName: 'Maria Rossi',
+        photoUser: 'https://picsum.photos/200',
+        date,
+        text: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quis nesciunt aut est commodi! Eos consequuntur, est expedita quo quaerat omnis illo praesentium nemo tempore facere, voluptas perspiciatis exercitationem unde alias!',
+        likes: 40
+    },
+    {
+        id,
+        fullName: 'Paolo Osella',
+        photoUser: 'https://picsum.photos/200',
+        date,
+        text: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quis nesciunt aut est commodi! Eos consequuntur, est expedita quo quaerat omnis illo praesentium nemo tempore facere, voluptas perspiciatis exercitationem unde alias!',
+        likes: 60,
+        image: 'https://picsum.photos/200',
+    }
+];
 
-console.log(arrayPost);
+//console.log(arrayPost);
 
 /** Funzione per creare la data di oggi in formato americano
  * 
@@ -47,3 +58,48 @@ function getDataAmericanFormat() {
     yyyy = date.getFullYear();
     return mm + gg + yyyy;
 }
+
+// genero le card con i relativi dati
+
+function generatePostCard(array) {
+    const containerPostElement = document.querySelector('.container');
+    array.forEach((element, i) => {
+        element.id = i;
+        //console.log(element.id);
+        let markupPost = `
+        <div class="post">
+            <div class="headerPost">
+                <img src="${element.photoUser}" alt="">
+                <div>
+                    <div class="name">${element.fullName}</div>
+                    <!-- /.name -->
+                    <div class="date">${element.date}</div>
+                    <!-- /.date -->
+                </div>
+            </div>
+            <!-- /.headerPost -->
+            <div class="mainPost">
+                <p class="text">${element.text}</p>
+                <!-- /.text -->
+                <img src="${element.image}" alt="" class="postImage">
+            </div>
+            <!-- /.mainPost -->
+            <div class="footerPost">
+                <div class="btnLike">
+                <i class="fa-solid fa-thumbs-up"></i>
+                Mi piace
+                </div>
+                <!-- /.btnLike -->
+                <div class="likeCounter">${element.likes}</div>
+                <!-- /.likeCounter -->
+            </div>
+            <!-- /.footerPost -->
+        </div>
+        <!-- /.post -->`;
+
+        containerPostElement.insertAdjacentHTML('beforeend', markupPost);
+    })
+
+}
+
+generatePostCard(arrayPost);
